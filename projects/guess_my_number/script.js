b4852776fcd3c792e2aca1421ba7c7d5e16b704e
RadosavPanic <radosav.panic01@gmail.com>
2024-05-13 12:25:26 +0200

    created selectors and event listener for guessing secret number

--- projects/guess_my_number/script.js
+++ projects/guess_my_number/script.js
@@ -1,5 +1,48 @@
 "use strict";
 
-let secretNumber = Math.trunc(Math.random() * 20) + 1;
+const labelMessage = document.querySelector(".message");
+const labelScore = document.querySelector(".score");
+const labelHighscore = document.querySelector(".highscore");
+const labelSecretNumber = document.querySelector(".number");
+
+const guessInput = document.querySelector(".guess");
+
+const btnCheck = document.querySelector(".check");
+const btnResetGame = document.querySelector(".again");
+
+const body = document.querySelector("body");
+
+const successColor = getComputedStyle(
+  document.documentElement
+).getPropertyValue("--success-color");
+
+let secretNumber = generateSecretNumber();
 let score = 20;
-let highscore = 20;
+let highscore = 0;
+
+function displayMessage(messageToDisplay) {
+  labelMessage.textContent = messageToDisplay;
+}
+
+function generateSecretNumber() {
+  const secretNumber = Math.trunc(Math.random() * 20) + 1;
+  return secretNumber;
+}
+
+btnCheck.addEventListener("click", () => {
+  const guess = Number(guessInput.value);
+
+  if (!guess) {
+    displayMessage("⛔ No Number!");
+  } else if (guess === secretNumber) {
+    displayMessage("🎉 Correct Number!");
+    labelSecretNumber.textContent = secretNumber;
+    body.style.backgroundColor = successColor;
+    labelSecretNumber.style.width = "30rem";
+
+    if (score > highscore) {
+      highscore = score;
+      labelHighscore.textContent = score;
+    }
+  }
+});
