cb56c865b1bb2199daaf9cb88fb544c97e96c98c
RadosavPanic <radosav.panic01@gmail.com>
2024-06-17 20:20:57 +0200

    working with getters, setters, static methods and object.create

--- oop/script.js
+++ oop/script.js
@@ -34,8 +34,54 @@ class PersonCl {
     // methods are created on prototype
     return new Date().getFullYear() - this.birthYear;
   }
+
+  get age() {
+    return this.calcAge();
+  }
+
+  static getClassName() {
+    return PersonCl.prototype.constructor.name;
+  }
 }
 
 const person2 = new PersonCl("Peter", 1985);
 console.log(person2); // PersonCl {firstName: 'Peter', birthYear: 1985}
 console.log(person2.calcAge()); // 39
+
+/* Getters and setters (available on object and classes) */
+const account = {
+  owner: "Peter",
+  movements: [200, 530, 120, 300],
+
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+
+  set latest(movement) {
+    this.movements.push(movement);
+  },
+};
+
+console.log(account.latest); // 300
+account.latest = 50;
+console.log(account.latest); // 50
+
+console.log(person2.age); // 39
+
+console.log(PersonCl.getClassName()); // static method on class
+
+/* Object.create */
+const PersonProto = {
+  calcAge() {
+    return new Date().getFullYear() - this.birthYear;
+  },
+
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
+};
+
+const person3 = Object.create(PersonProto);
+person3.init("Bob", 1979);
+console.log(person3.calcAge()); // 47
